79889e5b81b9443f0f091fce2b6b2eef1b20d922
diogokcn <diogo.kcn@gmail.com>
2017-06-25 20:50:55 -0300

    Adicionado template para Livro

--- _site/livro/views.py
+++ _site/livro/views.py
@@ -1,13 +1,14 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Livro
 
 def index(request):
-    todos_Livros = Livro.objects.all()
-    html = ''
-    for livro in todos_Livros:
-        url  = '/livro/' + str(livro.id) + '/'
-        html += '<a href="' + url + '">' + livro.titulo + '</a><br>'  
-    return HttpResponse(html)
+    todos_livros = Livro.objects.all()
+    template = loader.get_template('livro/index.html')
+    context  = {
+        'todos_livros': todos_livros,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, livro_id):
     return HttpResponse("<h2> Detalhes do Livro de ID " + str(livro_id) + "</h2>")
