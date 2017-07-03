f7d525c9043f9b2e4886bc7914b5a1ba77a19ae1
diogokcn <diogo.kcn@gmail.com>
2017-07-03 15:52:36 -0300

    User Delete Permission Added in DVD

--- _site/dvd/models.py
+++ _site/dvd/models.py
@@ -4,6 +4,8 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
+from django.conf import settings
 
 class DVD(models.Model):
     titulo  = models.CharField(verbose_name=_(u'Título'), max_length=50)
@@ -11,6 +13,7 @@ class DVD(models.Model):
     ano     = models.IntegerField(verbose_name=_('Ano'))
     duracao = models.IntegerField(verbose_name=_(u'Duração'))
     preco   = models.FloatField(verbose_name=_(u'Preço'))
+    user    = models.ForeignKey(settings.AUTH_USER_MODEL)
 
     def get_absolute_url(self):
         return reverse('dvd:detail', kwargs={'pk': self.pk}) 
--- _site/dvd/views.py
+++ _site/dvd/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
+from django.utils.translation import ugettext_lazy
 from .models import DVD
 
 class IndexView(generic.ListView):
@@ -21,7 +22,12 @@ class DetailView(generic.DetailView):
 class DVDCreate(CreateView):
 	model = DVD
 	fields = ['titulo', 'genero', 'ano', 'duracao', 'preco']
-
+	
+	def form_valid(self, form):
+		DVD = form.save(commit=False)
+		DVD.user = self.request.user
+		return super(DVDCreate, self).form_valid(form)
+		
 # Update
 class DVDUpdate(UpdateView):
 	model = DVD
